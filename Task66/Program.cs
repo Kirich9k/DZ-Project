3fd556e924a21f403ec2808c37ea7ea9234bacdf
Kirich9k <kirya65@gmail.com>
2023-01-09 18:34:50 +0300

    Task 66 accomplished

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int Summ(int m, int n)
+{
+	int start = m;
+	int end = n;
+	if(m > n)
+	{
+		start = n;
+		end = m;
+	}
+    else if(n == m)
+	{
+	    return m;
+	}
+	return (end + start)*(end - start + 1)/2;
+}
+		
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Summ(m, n));
