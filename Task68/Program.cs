ee9ce1cee6e5f53fe46da7c96c823d877945a08c
Kirich9k <kirya65@gmail.com>
2023-01-09 18:35:12 +0300

    Task 68 accomplished

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AkkermanFunc(int m, int n)
+{
+	if(m == 0)
+	{
+		return n + 1;
+	}
+	if(m > 0 && n == 0)
+	{
+		return AkkermanFunc(m - 1, 1);
+	}
+	return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AkkermanFunc(m, n));
